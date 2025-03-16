0e9f689c09339372871d2897813fa0edba7577c5
fishglitch <172350966+fishglitch@users.noreply.github.com>
2025-03-15 20:04:33 -0700

    WIP: implementation of tables in seed.js

--- server/seed.js
+++ server/seed.js
@@ -1,1 +1,13 @@
 // this is where you will seed starter data
+// use arrow functions (see 4.36 for the structure)
+
+
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://ealejo:fish70@localhost:5432/acme_auth_store_db');
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+
+const jwt = require('jsonwebtoken');
+const JWT = process.env.JWT || 'shhh';
+
+
